aa121b7d1fc375f6cf9c4257f20c8b3f7b277ed6
muqorrobiyn <muqorrobiyn@gmail.com>
2025-02-26 15:25:05 +0300

    add requirements.txt

--- manage.py
+++ manage.py
@@ -3,6 +3,8 @@
 import os
 import sys
 
+sys.path.append(os.path.dirname(os.path.abspath(__file__)))
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'config'))
 
 def main():
     """Run administrative tasks."""
